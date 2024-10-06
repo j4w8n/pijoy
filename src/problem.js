5a4a9fbd0b334c32f800a8629c7a00f69382e51a
Jason Creviston <the@j4w8n.com>
2024-10-06 13:41:52 -0400

    refactor: status references

--- src/problem.js
+++ src/problem.js
@@ -11,9 +11,9 @@ export const problem = (data) => {
   const { status, type, title, detail, instance, ...rest } = data
 
   /* Ensure status exists and is valid. */
-  if (!data.status) throw new Error('Member `status` must be passed into `problem()`.')
-  if (typeof data.status !== 'number') throw new Error('Member `status` must be a number.')
-  if (data.status < 100 || data.status > 599) throw new Error('Member `status` must be a number in the range of 100-599.')
+  if (!status) throw new Error('Member `status` must be passed into `problem()`.')
+  if (typeof status !== 'number') throw new Error('Member `status` must be a number.')
+  if (status < 100 || status > 599) throw new Error('Member `status` must be a number in the range of 100-599.')
   
   const code = http_codes.find(c => c.status === status) ?? { type: 'about:blank', title: 'Unknown Error' }
 
